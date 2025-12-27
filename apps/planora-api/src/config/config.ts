b969ab8a9a388198d9ee0001f51c4d3d7f9f4548
Phuc Nguyen Hoang <nhplhp2004@gmail.com>
2025-12-27 21:08:15 +0700

    BE: Add prisma client

--- apps/planora-api/src/config/config.ts
+++ apps/planora-api/src/config/config.ts
@@ -1,11 +1,12 @@
-import { makeValidators, num, Static, str } from 'nestjs-envalid';
+import { makeValidators, num, Static, str, url } from 'nestjs-envalid';
 
 export const validators = makeValidators({
   PORT: num({ default: 4000 }),
-  KEYCLOAK_SERVER_URL: str(),
+  KEYCLOAK_SERVER_URL: url(),
   KEYCLOAK_REALM: str(),
   KEYCLOAK_CLIENT_ID: str(),
   KEYCLOAK_SECRET: str(),
+  DATABASE_URL: url(),
 });
 
 export type Config = Static<typeof validators>;
